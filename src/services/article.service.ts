e8f382510b409204862aa41fe171683606f53001
viniciusaportela <vinicius.portela.stm@gmail.com>
2022-01-10 18:15:54 -0300

    Feat: add create and update transactions

--- src/services/article.service.ts
+++ src/services/article.service.ts
@@ -4,6 +4,9 @@ import { IArticle, IInputArticle } from "../typings/article.interface";
 import EventService from "./event.service";
 import LaunchService from "./launch.service";
 import ArticleModel from "../models/article.model";
+import ArticleEventModel from "../models/article-event.model";
+import ArticleLaunchModel from "../models/article-launch.model";
+import { postgresClient } from "../config/connect-db";
 
 export default class ArticleService {
   static async list(limit?: number, page?: number): Promise<IArticle[]> {
@@ -21,54 +24,85 @@ export default class ArticleService {
   }
 
   static async create(article: IInputArticle): Promise<IArticle> {
-    const isArticleInDb = await ArticleModel.has(article.id);
+    try {
+      await postgresClient.query('BEGIN');
+      const isArticleInDb = await ArticleModel.has(article.id);
 
-    if (!isArticleInDb) {
-      let inserted = await ArticleModel.create(article);
+      if (!isArticleInDb) {
+        let inserted = await ArticleModel.create(article);
 
-      if (article.events) {
-        await Promise.all(article.events.map(async event => {
-          await EventService.insertToArticle(event, inserted.id);
-        }))
+        if (article.events) {
+          await Promise.all(article.events.map(async event => {
+            await EventService.insertToArticle(event, inserted.id);
+          }))
+        }
+
+        if (article.launches) {
+          await Promise.all(article.launches.map(async launch => {
+            await LaunchService.insertToArticle(launch, inserted.id);
+          }))
+        }
+
+        if (article.events?.length || article.launches?.length) {
+          inserted = await ArticleModel.get(inserted.id);
+        }
+
+        await postgresClient.query('COMMIT');
+
+        return inserted;
+      } else {
+        throw new AlreadyExistsError();
       }
-
-      if (article.launches) {
-        await Promise.all(article.launches.map(async launch => {
-          await LaunchService.insertToArticle(launch, inserted.id);
-        }))
-      }
-
-      if (article.events?.length || article.launches?.length) {
-        inserted = await ArticleModel.get(inserted.id);
-      }
-
-      return inserted;
-    } else {
-      throw new AlreadyExistsError();
+    } catch(error) {
+      await postgresClient.query('ROLLBACK')
+      throw error;
     }
   }
 
   static async update(articleId: number, article: IInputArticle) {
-    const isArticleInDb = await ArticleModel.has(articleId);
+    try {
+      await postgresClient.query('BEGIN');
 
-    if (!isArticleInDb) {
-      throw new NotFoundError();
-    }
+      const articleInDb = await ArticleModel.get(articleId);
 
-    await ArticleModel.update(articleId, article);
-
-    // TODO remove old events and launches
-
-    if (article.events) {
-      for await (let event of article.events) {
-        await EventService.insertToArticle(event, articleId);
+      if (!articleInDb) {
+        throw new NotFoundError();
       }
-    }
 
-    if (article.launches) {
-      for await (let launch of article.launches) {
-        await LaunchService.insertToArticle(launch, articleId);
-      }
+      await ArticleModel.update(articleId, article);
+
+      const eventsToRemove = articleInDb.events.filter(event => !article.events.includes(event.id));
+      const eventsIdsToAdd = article.events.filter(eventId => 
+        articleInDb.events.findIndex(event => event.id === eventId) === -1
+      );
+
+      await Promise.all([
+        ...eventsToRemove.map(async (event) => {
+          await ArticleEventModel.delete(event.id, articleId);
+        }),
+        ...eventsIdsToAdd.map(async (eventId) => {
+          await EventService.insertToArticle(eventId, articleId);
+        })
+      ])
+
+      const launchesToRemove = articleInDb.launches.filter(launch => !article.launches.includes(launch.id));
+      const launchesToAdd = article.launches.filter(launchId => 
+        articleInDb.launches.findIndex(launch => launch.id === launchId) === -1
+      );
+
+      await Promise.all([
+        ...launchesToRemove.map(async (launch) => {
+          await ArticleLaunchModel.delete(launch.id, articleId);
+        }),
+        ...launchesToAdd.map(async (launchId) => {
+          await LaunchService.insertToArticle(launchId, articleId);
+        })
+      ])
+
+      await postgresClient.query('COMMIT');
+    } catch(error) {
+      await postgresClient.query('ROLLBACK');
+      throw error;
     }
   }
 
